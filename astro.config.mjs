560711b1df7529e6ec381fe950be183fbc2c386e
fermarinsanchez <fermarinsanchez@yahoo.es>
2025-08-21 20:33:10 +0200

    feat: Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://fermarinsanchez.github.io',
+    base: '/bbb25',
     integrations: [react()]
 });
